92a551c3d58ab9cc5fda76b9169dd7e098f54305
williamLafortuneCaissy <33548003+williamLafortuneCaissy@users.noreply.github.com>
2021-06-24 14:21:29 -0400

    Basic form setup using formik / yup

--- src/components/plan/edit/PlanEdit.js
+++ src/components/plan/edit/PlanEdit.js
@@ -1,18 +1,27 @@
 import { faPlus, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { setNestedObjectValues, useFormik } from 'formik';
 import React, { useState } from 'react';
 import { Button, Card, Col, Container, Form, Row } from 'react-bootstrap';
 import Header from '../../_global/header';
 import SetEdit from './SetEdit';
+import * as Yup from 'yup';
+
+const initialValues = {
+	planTitle: '',
+}
+const onSubmit = (values) => {
+	alert('form submitted check console')
+	console.log('form Data', values)
+}
+const validationSchema = Yup.object({
+	planTitle: Yup.string().required('This field is required')
+})
 
 // ===============================================================
 // this manages the add / remove / list Sets, as well as the title
 // ===============================================================
 const PlanEdit = () => {
-
-	// TODO: SAVE TITLE IN FAKE DATA, then process with exerciseEdit's form
-	const [title, setTitle] = useState(null)
-
 	const [sets, setSets] = useState([])
 
     // this simulates the next set id
@@ -29,14 +38,33 @@ const PlanEdit = () => {
         setSets(sets.filter(set => set !== id))
     }
 
+	// https://www.youtube.com/watch?v=PpdXB9Ru6oI&list=PLC3y8-rFHvwiPmFbtzEWjESkqBVDbdgGu
+	const formik = useFormik({
+		// short way of doing initialValues: initialValues
+		initialValues,
+		onSubmit,
+		// validate
+		validationSchema
+	})
+
 	return (
 		<div className="layout">
 			<Header title={'Create plan'} backHref="/"/>
 			<main className="layout-main container">
-				<Form>
+				<Form onSubmit={formik.handleSubmit}>
 					<Form.Group controlId="planTitle">
 						<Form.Label>Main Title</Form.Label>
-						<Form.Control type="text" placeholder="ex: My First Plan!"/>
+						<Form.Control
+							type="text"
+							placeholder="ex: My First Plan!"
+							value={formik.values.title}
+							onChange={formik.handleChange}
+							onBlur={formik.HandleBlur}
+						/>
+						{/* this will only show the error if the user just edited the input */}
+						{formik.touched.planTitle && formik.errors.planTitle &&
+							<div className="text-danger">{formik.errors.planTitle}</div>
+						}
 					</Form.Group>
 					{sets.map((id) => (
 						<SetEdit key={id} deleteSet={() => deleteSet(id)}/>
@@ -47,6 +75,7 @@ const PlanEdit = () => {
 							<FontAwesomeIcon icon={faPlus} />
 						</Button>
 					</div>
+					<Button type="submit" variant="light" block className="mt-3">Save</Button>
 				</Form>
 			</main>
 		</div>
