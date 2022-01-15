fe1ad5c5d02abd3a27577714f1fc6ebab36906ca
williamLafortuneCaissy <33548003+williamLafortuneCaissy@users.noreply.github.com>
2022-01-15 15:15:26 -0500

    Added delete plan

--- src/components/plan/list/PlanList.js
+++ src/components/plan/list/PlanList.js
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
-import { Button, Col, Container, Form, ListGroup, Row } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Button, Col, Container, Dropdown, Form, ListGroup, Row } from 'react-bootstrap';
 import { Link, useRouteMatch } from 'react-router-dom';
 import Header from '../../_global/header';
 import fakedata from '../../../data/dataPlan'
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const PlanList = () => {
 
@@ -13,22 +15,43 @@ const PlanList = () => {
     if (!localStorage.getItem("plans")) {
         localStorage.setItem("plans", JSON.stringify(fakedata))
     }
-    const data = JSON.parse(localStorage.getItem("plans")) || []
+    const [plans, setPlans] = useState(JSON.parse(localStorage.getItem("plans")) || []);
+
+    function handleDelete(planId) {
+        setPlans(prevPlan => prevPlan.filter(plan => plan.id != planId))
+    }
+
+    useEffect(() => {
+        localStorage.setItem("plans", JSON.stringify(plans))
+    }, [plans]);
 
     return (
         <div className="layout">
             <Header title={'Plans'} next="Create" nextHref={url + "/create"} />
             <main className="layout-main container">
                 <ListGroup>
-                    {data.map((plan, key) => (
+                    {plans.map((plan, key) => (
                         <ListGroup.Item key={key} className="bg-light">
                             <Form.Row>
-                                <Col xs="6" className="align-self-center"><h2 className="h5">{plan.title}</h2></Col>
-                                <Col xs="3">
-                                    <Link to={url + '/' + plan.id + '/view'} className="btn btn-block btn-outline-primary">View</Link>
-                                </Col>
-                                <Col xs="3">
+                                <Col xs="auto" className="align-self-center flex-grow-1"><h2 className="h5">{plan.title}</h2></Col>
+                                <Col xs="auto">
                                     <Link to={url + '/' + plan.id} className="btn btn-block btn-primary">Start</Link>
+                                </Col>
+                                <Col xs="auto">
+                                    {/* <Link to={url + '/' + plan.id + '/view'} className="btn btn-block btn-link">View</Link> */}
+                                    <Dropdown>
+                                        <Dropdown.Toggle variant="link" className="no-caret">
+                                            <FontAwesomeIcon icon={faEllipsisV} />
+                                        </Dropdown.Toggle>
+
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item as={Link} to={url + '/' + plan.id + '/view'}>View</Dropdown.Item>
+                                            {/* <Dropdown.Divider /> */}
+                                            {/* <Dropdown.Item href="#/action-3">Edit</Dropdown.Item> */}
+                                            <Dropdown.Divider />
+                                            <Dropdown.Item onClick={() => handleDelete(plan.id)}>Delete</Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
                                 </Col>
                             </Form.Row>
                         </ListGroup.Item>
