765ca3bebfaf56e07ef5040676247931da177bb0
williamLafortuneCaissy <33548003+williamLafortuneCaissy@users.noreply.github.com>
2024-05-04 15:02:37 -0400

    updated routing to hashRouter

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/trainingApp">
+    <HashRouter basename="/trainingApp">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
